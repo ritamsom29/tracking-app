0c7c8af0bf872c4e3ac94d3706ee6f47c07ab9a2
Ritam Som <ritamsom2004@gmail.com>
2026-03-06 12:56:26 +0530

    fix render routing

--- server/server.js
+++ server/server.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname + "/../public"));
+// Serve static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 io.on("connection", (socket) => {
-
-    socket.on("send-location", (data) => {
-
-        io.emit("receive-location", {
-            id: socket.id,
-            name: data.name,
-            latitude: data.latitude,
-            longitude: data.longitude
-        });
-
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", {
+      id: socket.id,
+      name: data.name,
+      latitude: data.latitude,
+      longitude: data.longitude
     });
-
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
