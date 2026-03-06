517fa532fd3e2b5d5b704731f4771bf5e8e88ad0
Ritam Som <ritamsom2004@gmail.com>
2026-03-06 12:51:14 +0530

    fix static path

--- server/server.js
+++ server/server.js
@@ -8,23 +8,23 @@ const io = new Server(server);
 
 app.use(express.static(__dirname + "/../public"));
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
-socket.on("send-location",(data)=>{
+    socket.on("send-location", (data) => {
 
-io.emit("receive-location",{
-id: socket.id,
-name: data.name,
-latitude: data.latitude,
-longitude: data.longitude
-});
+        io.emit("receive-location", {
+            id: socket.id,
+            name: data.name,
+            latitude: data.latitude,
+            longitude: data.longitude
+        });
 
-});
+    });
 
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-console.log("Server running on port " + PORT);
+    console.log("Server running on port " + PORT);
 });
